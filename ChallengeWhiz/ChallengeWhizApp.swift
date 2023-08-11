a33725188ff02c3e030767ec5faae4de79e5e143
Mayumi <mayumi.arakakisiccha@gmail.com>
2023-08-11 10:36:41 -0500

    Initial Commit

---
+++ ChallengeWhiz/ChallengeWhizApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeWhizApp.swift
+//  ChallengeWhiz
+//
+//  Created by Enrique Alata Vences on 11/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeWhizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
